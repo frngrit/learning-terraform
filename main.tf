c3b2111e1e35f72835d05941e8159242823f68ce
rerngrit-frank <frank.12876@hotmail.com>
2024-01-03 21:40:31 +0700

    feat: add comment and desc

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ resource "aws_instance" "blog" {
 
 resource "aws_security_group" "blog" {
   name = "blog"
+  description = "Allow HTTP/HTTPS in, Allow everything out"
   tags = {
     Terraform = "true"
   }
@@ -41,7 +42,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"] ##every ip##
   security_group_id = aws_security_group.blog.id
 }
 
@@ -51,7 +52,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"] ##every ip##
   security_group_id = aws_security_group.blog.id
 }
 
@@ -60,7 +61,7 @@ resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  protocol    = "-1" ##every protocal##
+  cidr_blocks = ["0.0.0.0/0"] ##every ip##
   security_group_id = aws_security_group.blog.id
 }
