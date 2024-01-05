d592405b1d7be216187ba5674a85baa248dbe345
rerngrit-frank <frank.12876@hotmail.com>
2024-01-05 21:29:15 +0700

    fix: fix target group

--- main.tf
+++ main.tf
@@ -58,6 +58,9 @@ module "alb" {
 
 
   name            = "blog-alb"
+
+  load_balancer_type = "application"
+
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
@@ -68,12 +71,6 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
-      }
     }
   ]
 
@@ -86,7 +83,6 @@ module "alb" {
   ]
 
   tags = {
-    Environment = "Development"
-    Project     = "Example"
+    Environment = "dev"
   }
 }
